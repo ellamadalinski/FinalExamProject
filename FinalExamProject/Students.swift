d90e1f7b29db52d21b472c7fbb7438fcbd324f71
Ella Madalinski <ella.madalinski24@d155.org>
2021-11-30 08:00:52 -0600

    students class outline made - testing

---
+++ FinalExamProject/Students.swift
@@ -0,0 +1,16 @@
+//
+//  Students.swift
+//  FinalExamProject
+//
+//  Created by ELLA MADALINSKI on 11/30/21.
+//
+
+import Foundation
+
+class Students{
+    
+    init(){
+        
+    }
+    
+}
