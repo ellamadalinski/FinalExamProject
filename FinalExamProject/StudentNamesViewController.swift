c5331c61e2f83430dec157d6db7020b9b780a570
Daniel Vega <daniel.vega23@d155.org>
2021-12-07 08:12:17 -0600

    alert controller

--- FinalExamProject/StudentNamesViewController.swift
+++ FinalExamProject/StudentNamesViewController.swift
@@ -12,7 +12,7 @@ class StudentNamesViewController: UIViewController , UITableViewDelegate , UITab
 
     @IBOutlet weak var tableViewOutlet: UITableView!
     var incomingClass = Class(c: "default", cp: "default", sa: [Students]())
-    
+    var whichClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,10 +64,54 @@ class StudentNamesViewController: UIViewController , UITableViewDelegate , UITab
     }
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
        // when you click on another cell it deselects the previous cell you were clicked on
-    
+       
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        whichClicked = indexPath.row
         performSegue(withIdentifier: "studentsToStudentInfo", sender: nil)
     }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            incomingClass.studentsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.reloadData()
+          
+        }
+    }
     
+    @IBAction func cellHeld(_ sender: UILongPressGestureRecognizer) {
+       
+        
+        let alertController = UIAlertController(title: "Change Name", message: nil, preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Change Name"
+            
+        }
+        
+        
+        let changeNameAction = UIAlertAction(title: "Add", style: .default) { [weak alertController] _ in guard let textFields = alertController?.textFields else { return }
+                                            
+                if let studentName = textFields[0].text {
+                                                
+                    self.incomingClass.studentsArray.append(Students(n: studentName))
+                    
+                }
+            self.tableViewOutlet.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let goToStudentsScreen = UIAlertAction(title: "Go to student screen", style: .default) { blah in
+            self.performSegue(withIdentifier: "goToStudents", sender: nil)
+        }
+       
+        alertController.addAction(cancelAction)
+        alertController.addAction(changeNameAction)
+        
+        self.present(alertController,
+                     animated: true)
+        
+     
+    
+    
+}
 }
