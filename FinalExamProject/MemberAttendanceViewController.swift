57e3b98057a1e8c5d01b969c357b41e2bb9abf5a
Daniel Vega <drvega7@icloud.com>
2021-12-14 21:29:13 -0600

    all the attendances are working

--- FinalExamProject/MemberAttendanceViewController.swift
+++ FinalExamProject/MemberAttendanceViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
-class MemberAttendanceViewController: UIViewController {
-
+class MemberAttendanceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
+    
+    var newIncomingMember = Club(c: "", sa: [Students]())
+    var global = [IndexPath]()
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        self.tableViewOutlet.allowsMultipleSelection = true
+        self.tableViewOutlet.allowsMultipleSelectionDuringEditing = true
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newIncomingMember.studentsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = newIncomingMember.studentsArray[indexPath.row].name
+        global.append(indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        newIncomingMember.studentsArray[indexPath.row].absent = true
+        
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        newIncomingMember.studentsArray[indexPath.row].absent = false
+    }
+    
+    @IBAction func markAbsentAction(_ sender: UIButton) {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let currentDate = dateFormatter.string(from: date)
+        var index = 0
+        while(index < newIncomingMember.studentsArray.count) {
+            if newIncomingMember.studentsArray[index].absent {
+                newIncomingMember.studentsArray[index].daysMissed.append(currentDate)
+            }
+            else {
+                
+            }
+            index += 1
+        }
+        var yuh = 0
+        var insideYuh = 0
+        while(yuh < newIncomingMember.studentsArray.count) {
+            while(insideYuh < global.count) {
+             
+                tableViewOutlet.deselectRow(at: global[insideYuh], animated: true)
+                insideYuh += 1
+            }
+            newIncomingMember.studentsArray[yuh].absent = false
+            yuh += 1
+          
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- FinalExamProject/PlayerAttendanceViewController.swift
+++ FinalExamProject/PlayerAttendanceViewController.swift
@@ -7,25 +7,69 @@
 
 import UIKit
 
-class PlayerAttendanceViewController: UIViewController {
-
-    var newIncomingAthletic = Athletic(a: "", sa: [Students](), s: "")
+class PlayerAttendanceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var newIncomingAthletic = Athletic(a: "", sa: [Students](), s: "")
+    var global = [IndexPath]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        self.tableViewOutlet.allowsMultipleSelection = true
+        self.tableViewOutlet.allowsMultipleSelectionDuringEditing = true
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newIncomingAthletic.studentsArray.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = newIncomingAthletic.studentsArray[indexPath.row].name
+        global.append(indexPath)
+        return cell
     }
-    */
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        newIncomingAthletic.studentsArray[indexPath.row].absent = true
+        
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        newIncomingAthletic.studentsArray[indexPath.row].absent = false
+    }
+    
+    @IBAction func markAbsentAction(_ sender: UIButton){
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let currentDate = dateFormatter.string(from: date)
+        var index = 0
+        while(index < newIncomingAthletic.studentsArray.count) {
+            if newIncomingAthletic.studentsArray[index].absent {
+                newIncomingAthletic.studentsArray[index].daysMissed.append(currentDate)
+            }
+            else {
+                
+            }
+            index += 1
+        }
+        var yuh = 0
+        var insideYuh = 0
+        while(yuh < newIncomingAthletic.studentsArray.count) {
+            while(insideYuh < global.count) {
+             
+                tableViewOutlet.deselectRow(at: global[insideYuh], animated: true)
+                insideYuh += 1
+            }
+            newIncomingAthletic.studentsArray[yuh].absent = false
+            yuh += 1
+          
+        }
+        
+    }
+    
+  
 }
